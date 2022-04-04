435f69d6747dfcd5b0968b1282472ac20960b302
JamesNg <njames15@gmail.com>
2022-04-04 10:24:26 -0400

    added intro & fun-interactive section

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector('body').appendChild(h2);
+const hello = document.querySelector('h1');
+hello.addEventListener('click', function() {
+    alert('How are you?');
+}); 
+
+const fun = document.querySelector('td#click');
+fun.addEventListener('click', function() {
+    alert('Cool stuff eh?');
+});
+
+const hover = document.getElementById('hover');
+hover.addEventListener('mouseenter', function() {
+    alert("You're a helicopter!");
+});
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowUp") {
+        alert('Yup!');
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowDown") {
+        alert(':(');
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowRight") {
+        alert('<(*-*)^');
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowLeft") {
+        alert('Chef?');
+    }
+});
